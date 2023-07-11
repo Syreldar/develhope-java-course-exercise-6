0a87ad8fc3cfc36b90b06e5b040ece6c23b0e8f2
Syreldar <enrico.drago3@gmail.com>
2023-07-11 13:16:30 +0200

    Upload exercise

--- StringHandling.java
+++ StringHandling.java
@@ -2,5 +2,15 @@ public class StringHandling
 {
     public static void main(String[] args)
     {
+        String string1 = "Hello";
+        String string2 = "How are you?";
+
+        char[] combinationArr = new char[4];
+        combinationArr[0] = string1.charAt(0);
+        combinationArr[1] = string1.charAt(1);
+        combinationArr[2] = string2.charAt(string2.length() - 2);
+        combinationArr[3] = string2.charAt(string2.length() - 1);
+
+        System.out.println(combinationArr);
     }
 }
